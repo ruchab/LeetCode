2dcfc10d11a80a971cef5c3607c14add7007fa75
ruchab <rucha.b@gmail.com>
2015-06-20 21:29:11 -0700

    first commit

---
+++ DataStructures&Algorithms/src/arrays/rotateArray.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Rucha on 6/20/15.
+ * Rotate an array of n elements to the right by k steps.
+ *For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+ */
+public class rotateArray {
+
+    public static void main(String[] args){
+
+       int[]arr= {1,2,3,4};
+       int n = 3;
+       rotateArray(arr,n);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void rotateArray(int[] arr, int n){
+        n %=arr.length; //if n i greater than arr.length then the resulting shift is n%arr.length
+
+        reverse(arr,0,arr.length-1); //reverse whole array
+        reverse(arr,0,n-1);         //reverse from 0 to n-1
+        reverse(arr,n,arr.length-1); //reverse from n to end
+    }
+
+    public static void reverse(int[] arr,int left,int right){
+        if(arr == null || arr.length==1) return;
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
+
+}
