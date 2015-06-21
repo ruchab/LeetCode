99d4f00f74997f36eb3b15841a6bc3c65b200011
ruchab <rucha.b@gmail.com>
2015-06-21 01:27:34 -0700

    no message

---
+++ DataStructures&Algorithms/src/arrays/twoSum.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by Rucha on 6/21/15.
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+ The function twoSum should return indices of the two numbers such that they add up to the target,
+ where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ You may assume that each input would have exactly one solution.
+ Input: numbers={2, 7, 11, 15}, target=9
+ Output: index1=1, index2=2
+ */
+public class twoSum {
+
+    public static void main (String[] args){
+        int[] n = {1,2,3,4,5,6,7};
+        int target = 3;
+        int[] result = new int[2];
+        result = twoSum(n,target);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    public static int[] twoSum(int[] numbers, int target){
+        int[] result = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<numbers.length;i++){
+            if(map.containsKey(numbers[i])){
+                result[0] = map.get(numbers[i]);
+                result[1] = i+1;
+            }else{
+                map.put(target-numbers[i],i+1); //non zero based index
+            }
+        }
+        return result;
+    }
+}
