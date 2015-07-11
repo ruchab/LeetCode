77b4973f546555ab08739c1d2241e69e89e574fe
ruchab <rucha.b@gmail.com>
2015-07-11 11:24:28 -0700

    no message

--- DataStructures&Algorithms/src/Tree/BinaryTreeLevelOrderTraversal.java
+++ DataStructures&Algorithms/src/Tree/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,39 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Created by Rucha on 7/11/15.
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
  */
 public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> tempResult = new ArrayList<Integer>();
+
+            for(int i=0;i<size;i++){
+                TreeNode temp = queue.poll();
+                tempResult.add(temp.val);
+                if(temp.left!=null) queue.add(temp.left);
+                if(temp.right!=null) queue.add(temp.right);
+            }
+            result.add(tempResult);
+        }
+
+            return result;
+
+
+    }
+
 }
